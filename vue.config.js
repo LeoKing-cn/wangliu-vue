f8fe20f1ddd5a398c12e337549be3b0a33c89cc3
wangliu <593486184@qq.com>
2021-08-11 17:34:59 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.ts",
+      template: "public/index.html",
+      filename: "index.html"
+    }
+  },
+  css: {
+    extract: false //强制使用内联样式，这样打包出的插件样式会内置在js中，不需要单独引入
+  },
+  devServer: {
+    port: 80
+  }
+};
